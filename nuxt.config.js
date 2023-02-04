edab7a6496bc70f4abee288933ed258ccf79be49
Anurag Kumar <65001074+anuragkumar19@users.noreply.github.com>
2023-02-05 00:57:42 +0530

    img compression test

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "https://memebook-r4vf.onrender.com/api/v1/",
+    baseURL: "https://memebook-pr-19.onrender.com/api/v1/",
   },
 
   toast: {
